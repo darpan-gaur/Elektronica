65336b92a7d366a643ac40fde2a05fee0d211358
aaryan200 <co21btech11001@iith.ac.in>
2023-04-23 17:54:39 +0530

    commit

--- ChessBot/utils.py
+++ ChessBot/utils.py
@@ -1,12 +1,37 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential, Functional, Model, load_model
+from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+from PIL import Image
+import subprocess
+import itertools
+import os
+import shutil
+import random
+import glob
+import matplotlib.pyplot as plt
+from os import listdir
+from keras.applications import imagenet_utils
 import cv2
-import joblib
+import warnings
+from keras.preprocessing import image
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 m = ['a','b','c','d','e','f','g','h']
 
-# PATH_TO_MODEL = 'model.pkl'
-# model = joblib.load(PATH_TO_MODEL)
+PATH_TO_MODEL = 'ChessPieceDetection/model.h5'
+model = load_model(PATH_TO_MODEL)
 
 def crop_img(path_to_image):
     img = cv2.imread(path_to_image)
+    img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    img = cv2.resize(img, (512, 512))
+    img = img[43:362, 60:480]
     img = cv2.resize(img, (512, 512))
     out = []
     for i in range(8):
@@ -19,7 +44,14 @@ def crop_img(path_to_image):
 def get_array(path_to_photo):
     # 5 dimensional array of 64x(size of one cropped image)
     imgs = crop_img(path_to_photo)
-    arr = [[model.predict(imgs[i][j]) for j in range(8)] for i in range(8)]
+    li = ['B', 'a', 'W']
+    for i in range(8):
+        for j in range(8):
+            imgs[i][j] = Image.fromarray(imgs[i][j].astype('uint8'), 'RGB')
+            imgs[i][j] = imgs[i][j].resize((224, 224))
+            imgs[i][j] = tf.keras.applications.mobilenet.preprocess_input(np.array(imgs[i][j], dtype=np.float32))
+            imgs[i][j] = np.expand_dims(imgs[i][j], axis=0)
+    arr = [[li[model.predict(imgs[i][j])] for j in range(8)] for i in range(8)]
     # for i in range(8):
     #     temp = []
     #     for j in range(8):
@@ -27,6 +59,33 @@ def get_array(path_to_photo):
     #     arr.append(temp)
     return arr
 
+def click_photo():
+    subprocess.call("rm -rf img/*", shell=True)
+    cam_port = 0
+    cam = cv2.VideoCapture(cam_port)
+
+    # reading the input using the camera
+    result, image = cam.read()
+
+    # If image will detected without any error,
+    # show result
+    if result:
+
+        # showing result, it take frame name and image
+        # output
+        cv2.imshow("img", image)
+
+        # saving image in local storage
+        cv2.imwrite("imgs/1.png", image)
+
+        # If keyboard interrupt occurs, destroy image
+        # window
+        cv2.waitKey(10)
+        cv2.destroyWindow("img")
+
+    # If captured image is corrupted, moving to else part
+    else:
+        print("No image detected. Please! try again")
 
 def move_from_player(arr, prev):
     changes = []
